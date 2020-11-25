0dcd6d78bf0cb476a1364694751b9eda6befe129
NalessoAxel <axelnalesso@gmail.com>
2020-11-25 11:05:27 +0100

    ajout timer and some style

--- maze.js
+++ maze.js
@@ -1,107 +1,145 @@
+const level1 = `***********.*
+*S.....**.*.T
+*****.....*.* 
+*****.***.*.* 
+*****.*****.* 
+*****.*****.* 
+*****.......* 
+*****.******* 
+*.........*** 
+*.******...** 
+*....********`
+
+
 const body = document.querySelector('body')
-body.className = 'body'
-const wall = document.createElement('div')
-wall.className = 'wall'
-const paths = document.createElement('div')
-paths.className = 'paths'
-const start = document.createElement('div')
-start.className = 'start'
-const tresor = document.createElement('div')
-tresor.className = 'Tresor'
+body.classList.add('body')
+const main = document.createElement('main')
+const time = document.createElement('div')
+time.classList.add('time')
+const p = document.createElement('p');
+p.classList.add('timer')
+body.appendChild(main)
 
+let seconds = 0;
+let minutes = 0;
 
-
-
-
-
-
-const multiline = `***********.**S.....**.*.T*****.....*.******.***.*.******.*****.******.*****.******.......******.********.........****.******...***....********`
-
-console.log(multiline.split(' ')) // ['line1", 'line2', 'line3']
-console.log(multiline[0].split('')) // ['l', 'i', 'n', 'e', '1']
-
-
-
-for (let i = 0; i < multiline.length; i++) {
-    const line = document.createElement('div')
-    //line.innerHTML = multiline[i].split('')
-    line.className = 'ligne'
-
-
-
-    if (multiline[i] == '*') {
-        line.className = 'wall'
+function timer() {
+    const para = document.querySelector('p')
+    if (seconds >= 0) {
+        para.textContent = 'time ' + seconds + ' s'
+        if (seconds > 60) {
+            para.textContent = 'time ' + minutes + ' mn ' + (seconds - (minutes * 60)) + ' s'
+        }
     }
-    if (multiline[i] == '.') {
-        line.className = 'paths'
+    if (seconds % 60 === 0 && seconds !== 0) {
+        para.textContent = (minutes + 1) + ' mn'
+        minutes++
     }
-    if (multiline[i] == 'S') {
-        line.className = 'Start'
-    }
-    if (multiline[i] == 'T') {
-        line.className = 'Tresor'
-    }
+    seconds++
+}
+const interval = setInterval(timer, 1000)
 
-    body.appendChild(line)
-    line.appendChild(start)
 
+
+const lineArr = level1.split('\n')
+
+for (let i = 0; i <= lineArr.length - 1; i++) {
+    const lineDiv = document.createElement('div')
+    lineArr[i] == '*'
+    lineDiv.classList.add('lineDiv')
+    const line = lineArr[i]
+    for (let j = 0; j <= line.length - 1; j++) {
+        const characterDiv = document.createElement('div');
+        const character = line.split('');
+        characterDiv.innerHTML = character[j];
+        lineDiv.appendChild(characterDiv)
+        if (characterDiv.innerHTML == "*") {
+            characterDiv.classList.add('wall')
+            characterDiv.classList.add('tile')
+            characterDiv.innerHTML = ''
+        } else if (characterDiv.innerHTML == '.') {
+            characterDiv.classList.add('paths')
+            characterDiv.classList.add('tile')
+            characterDiv.innerHTML = ''
+        } else if (characterDiv.innerHTML == "S") {
+            characterDiv.classList.add("start")
+            characterDiv.classList.add('tile')
+            characterDiv.innerHTML = ''
+        } else if (characterDiv.innerHTML == "T") {
+            characterDiv.classList.add("tresor")
+            characterDiv.classList.add('tile')
+            characterDiv.innerHTML = ''
+            //tresorPosX = character.indexOf('T')
+            //tresorPosY = i
+        }
+    }
+    main.appendChild(lineDiv)
 }
 
+
+
+main.appendChild(time)
+time.appendChild(p)
 const player = document.createElement('div')
-player.className = 'player'
-document.querySelector("body > div.Start").appendChild(player)
+player.classList.add('player')
+document.querySelector("body > main > div:nth-child(2) > div.start.tile").appendChild(player)
 
-let positionX = 17
+let posX = 2
+//posX += tresorPosX +1
+let posY = 2
+//posY += tresorPosY +1
 
 
-document.addEventListener('keydown', function (e) {
-    if (e.key === 'ArrowRight') {
+document.body.addEventListener('keydown', function (e) {
+    if (e.code === 'ArrowLeft') {
         console.log(e)
-        positionX++
-        if (document.querySelector('body > div:nth-child(' + positionX + ')').classList.contains('wall')) {
-            positionX -= 1
-            document.querySelector('body > div:nth-child(' + positionX + ')').appendChild(player)
+
+        if (document.querySelector('body > main > div:nth-child(' + posY + ')> div:nth-child(' + (posX - 1) + ')').classList.contains('wall')) {
+            console.log('Hit a wall bolosse!')
+
+
         } else {
-            document.querySelector('body > div:nth-child(' + positionX + ')').appendChild(player)
+            posX--
+            document.querySelector('body > main > div:nth-child(' + posY + ')> div:nth-child(' + posX + ')').appendChild(player)
         }
-
     }
-    if (e.key === 'ArrowLeft') {
-        console.log(e)
-        positionX--
-        if (document.querySelector('body > div:nth-child(' + positionX + ')').classList.contains('wall')) {
-            positionX += 1
-            document.querySelector('body > div:nth-child(' + positionX + ')').appendChild(player)
+
+    if (e.code === 'ArrowRight') {
+        if (document.querySelector('body > main > div:nth-child(' + posY + ')> div:nth-child(' + (posX + 1) + ')').classList.contains('wall')) {
+            console.log('Hit a wall bolosse!')
+
         } else {
-            document.querySelector('body > div:nth-child(' + positionX + ')').appendChild(player)
+            posX++
+            document.querySelector('body > main > div:nth-child(' + posY + ')> div:nth-child(' + posX + ')').appendChild(player)
         }
-
-
     }
-    if (e.key === 'ArrowUp') {
-        console.log(e)
-        positionX -= 13
-        if (document.querySelector('body > div:nth-child(' + positionX + ')').classList.contains('wall')) {
-            positionX += 13
-            document.querySelector('body > div:nth-child(' + positionX + ')').appendChild(player)
-        } else {
-            document.querySelector('body > div:nth-child(' + positionX + ')').appendChild(player)
-        }
+    if (e.code === 'ArrowUp') {
+       /*  if (posY >= lineArr.length) {
+            posY-- */
+            if (document.querySelector('body > main > div:nth-child(' + (posY - 1) + ')> div:nth-child(' + (posX) + ')').classList.contains('wall')) {
+                console.log('Hit a wall bolosse!')
 
+            } else {
+                posY--
+                document.querySelector('body > main > div:nth-child(' + posY + ')> div:nth-child(' + posX + ')').appendChild(player)
+            }
+        
     }
-    if (e.key === 'ArrowDown') {
-        console.log(e)
-        positionX += 13
-        if (document.querySelector('body > div:nth-child(' + positionX + ')').classList.contains('wall')) {
-            positionX -= 13
-            document.querySelector('body > div:nth-child(' + positionX + ')').appendChild(player)
-        } else {
-            document.querySelector('body > div:nth-child(' + positionX + ')').appendChild(player)
-        }
+    if (e.code === 'ArrowDown') {
+        /* if (posY <= lineArr.length) {
+            posY++ */
+            if (document.querySelector('body > main > div:nth-child(' + (posY + 1) + ')> div:nth-child(' + posX + ')').classList.contains('wall')) {
+                console.log('Hit a wall bolosse!')
 
+            } else {
+                posY++
+                document.querySelector('body > main > div:nth-child(' + posY + ')> div:nth-child(' + posX + ')').appendChild(player)
+            }
+        
     }
-    if(document.querySelector('body > div:nth-child(' + positionX + ')').classList.contains('Tresor')) {
-        alert('You win!')
-    }
+    /* if (document.querySelector("body > main > div:nth-child(" + posY + ") > div.tresor > div")){
+        alert ('You Won Little Lucky Guy ! ')
+        }  */
+    
 
 })
